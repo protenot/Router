f2e80d255ff068a799d7c64e9b8dc091f22cca22
Olga_Belaya <protenot@yandex.ru>
2023-08-15 10:11:42 +0300

    install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "jest/globals": true,
+    },
+    "extends": [
+        //"standard-with-typescript",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended"
+
+],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    plugins: ["jest", "import"],
+    "rules": {
+        "no-undef":"off",
+        
+        "import/prefer-default-export": "off",
+        "import/no-unresolved": "off",
+        "import/extensions": ["warn", "never"],
+        "@typescript-eslint/prefer-nullish-coalescing": "off",
+        "@typescript-eslint/strict-boolean-expressions": "off",
+    }
+}
